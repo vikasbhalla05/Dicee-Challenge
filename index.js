2ba17c2f02ae8b9a7599ecd5c0c11676fd504a24
vikasbhalla05 <77003706+vikasbhalla05@users.noreply.github.com>
2021-08-24 15:51:13 +0530

    Tried fixing the flag bug

--- index.js
+++ index.js
@@ -17,10 +17,8 @@ if(randomNumber1==randomNumber2){
     document.querySelector("h1").textContent="Draw Both Players have half 😉";
 }
 else if(randomNumber1>randomNumber2){
-    document.querySelector("h1").textContent="Player 1 Wins!";
-    document.querySelectorAll(".dice>p")[0].textContent="Player 1 🚩";
+    document.querySelector("h1").textContent="Player 1 Wins! 🚩";
 }
 else{
-    document.querySelector("h1").textContent="Player 2 Wins!";
-    document.querySelectorAll(".dice>p")[1].textContent="Player 2 🚩";
+    document.querySelector("h1").textContent="Player 2 Wins! 🚩";
 }
