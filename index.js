ab4b9e0954cbc58e2fb9727cfdfe6dc6c0df7480
vikasbhalla05 <77003706+vikasbhalla05@users.noreply.github.com>
2021-08-24 12:44:53 +0530

    changed directories once again, something is fishy

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // numbering for dice 1
 var randomNumber1=Math.floor(Math.random()*6)+1;
 var randomDiceImg1="dice"+randomNumber1+".png";
-var randomImgSrc1="/images/"+randomDiceImg1;
+var randomImgSrc1="images/"+randomDiceImg1;
 var Img1=document.querySelectorAll("img")[0];
 Img1.setAttribute("src",randomImgSrc1);  
 
 // numbering for dice 2
 var randomNumber2=Math.floor(Math.random()*6)+1;
 var randomDiceImg2="dice"+randomNumber2+".png";
-var randomImgSrc2="/images/"+randomDiceImg2;
+var randomImgSrc2="images/"+randomDiceImg2;
 var Img2=document.querySelectorAll("img")[1];
 Img2.setAttribute("src",randomImgSrc2);
